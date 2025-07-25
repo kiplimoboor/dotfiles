2ec5c9ec078d26d9a6ece59c0fbbea1e1782387f
kiplimoboor <kiplimoarapboor@gmail.com>
2025-07-25 11:39:46 +0300

    custom init config with kickstart base

---
+++ install_nvim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt purge neovim -y
+sudo apt autoremove -y
+
+rm -rf ~/.local/share/nvim
+rm -rf ~/.config/nvim/
+
+git clone --depth 1 -b stable https://github.com/neovim/neovim
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build
+cpack -G DEB
+sudo dpkg -i nvim-linux-x86_64.deb
+
+cd ..
+cd ..
+rm -rf neovim
+
+mkdir ~/.config/nvim
+cd ~/.config/nvim/
+
+wget https://raw.githubusercontent.com/kiplimoboor/dotfiles/refs/heads/main/init.lua
