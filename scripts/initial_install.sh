718ff86568284be1ecd6cc1dd81b9f992708285f
kiplimoboor <kiplimoarapboor@gmail.com>
2025-12-21 21:51:43 +0300

    add initial helper script

--- scripts/initial_install.sh
+++ scripts/initial_install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --needed alacritty brightnessctl hyprlock hyprpaper impala less ttf-jetbrains-mono-nerd neovim swayosd unzip waybar wl-clipboard
+
+cp -rf ~/dotfiles/.config/* ~/.config
+cp ~/dotfiles/shell/.bashrc ~/.bashrc
+
+sudo cp -rf ~/dotfiles/usr/share/sddm/themes/omarchy /usr/share/sddm/themes/
+sudo cp ~/dotfiles/etc/sddm.conf /etc/
+sudo cp ~/dotfiles/scripts/hypr_toggle_theme.sh /bin/toggle-theme
+sudo reboot now
